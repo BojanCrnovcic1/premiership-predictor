9b7b40c6ff918479fba919d67023f784283ba71e
Bojan Crnovcic <bojan969@gmail.com>
2026-08-15 18:32:52 +0200

    lc

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import styles from "./Logo.module.scss";
+import styles from "./logo.module.scss";
 
 export const Logo = () => {
   const navigate = useNavigate();
